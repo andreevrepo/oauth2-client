3ef2be82cc6781dc77fe7adf4b0cdacfdca90399
Andrew <korskors144@gmail.com>
2018-04-12 12:27:57 +0300

    test

--- src/main/java/net/takedata/spring/Application.java
+++ src/main/java/net/takedata/spring/Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-/*
-Some changes2
- */
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
