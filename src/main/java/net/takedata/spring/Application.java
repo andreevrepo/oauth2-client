7615e02425d261c52921b1f08879704248013c59
Andrew <korskors144@gmail.com>
2018-04-12 11:07:10 +0300

    changes

--- src/main/java/net/takedata/spring/Application.java
+++ src/main/java/net/takedata/spring/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+/*
+Some changes
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
